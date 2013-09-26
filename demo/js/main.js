a3f7bbdcc820d1dff8ce75a3d2f092bfcdd14341
Steve Palmer <steve@Steak.local>
2013-09-25 23:33:15 -0700

    Added demo to pages

---
+++ demo/js/main.js
@@ -0,0 +1,73 @@
+jQuery(document).ready(function($) {
+	
+
+	// default	
+	$("#billboard1")
+		.billboard();
+			
+	// transition to right with controls		
+	$("#billboard2")
+		.billboard({ 
+			transition:		"right", 
+			duration:			2000, 
+			speed: 				500, 
+			//navType:			"controls",
+			autosize: 		false	
+		});
+	
+	// transition up with no navigation	
+	$("#billboard3")
+		.billboard({ 
+			transition:		"up", 
+			navType:			"none",
+			stretch:			false 
+		});
+	
+	// transition down with custom easing	
+	$("#billboard4")
+		.billboard({ 
+			ease:					"easeInOutElastic", 
+			speed:				2000, 
+			transition:		"down"
+		});
+
+	// fade transition with no footer and resize to each slide's content
+	$("#billboard5")
+		.billboard({ 
+			transition:		"fade", 
+			includeFooter: false,
+			resize:				 true 
+		});
+	
+	// callback for slide change
+	$("#billboard6")
+		.billboard({
+			onSlideChange: slideChangeHandler
+		});
+		
+	function slideChangeHandler( curSlide, prevSlide, reverse, args )
+	{
+		var 
+			slide = this.billboard().get(curSlide);
+
+		// do something
+	}		
+
+	// controls for last billboard
+	$("nav.goto a")
+		.click(function(e) {
+			e.preventDefault();
+			
+			$("#billboard6")
+				.billboard("goto", [ $("nav.goto a").index(this) ]);
+		
+		});
+		
+	// pretty printed code
+	$("pre.prettyprint")
+		.each(function() {
+			$(this)
+				.text( $(this).html() );
+		});
+	
+});
